1e875bee2d97dc4e72cad2fd4e421b9ca6318690
Matheus <morais.matheus.web@gmail.com>
2022-03-03 17:02:14 -0300

    Initial architecturing

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+
+import router from "./routes/index.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Server is listening on port ', process.env.PORT)
+});
