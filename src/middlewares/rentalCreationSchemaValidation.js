72f04a45581028485ca29ee58a5b8033d8970e82
Liaess <yann.melo@gmail.com>
2022-03-10 11:12:53 -0300

    Preparando revisão de código

--- src/middlewares/rentalCreationSchemaValidation.js
+++
@@ -1,8 +0,0 @@
-import rentalCreationSchema from "../schemas/rentalCreationSchema.js";
-
-export default function rentalCreationSchemaValidation(req, res, next) {
-
-    const validation = rentalCreationSchema.validate(req.body);
-    if(validation.error) return res.sendStatus(400);
-    next();
-}
